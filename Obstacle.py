e37de8b19e8f16189fd6b42a5d73cbdb9526d32b
emiliosao <emilio_sao@icloud.com>
2021-05-18 23:10:15 +0200

    more "fixes"

--- Obstacle.py
+++ Obstacle.py
@@ -7,21 +7,34 @@ class VirtualObstacle(pygame.sprite.Sprite):
     """Virtual parent class of obstacle that has all the same variables and functions
     except anything required with drawing to screen."""
 
+    # constructor
     def __init__(self, DISPLAYSURF, speed, width, height, center):
-        super().__init__()
+        super().__init__()  # initialise from the parent class
         self.DISPLAYSURF = DISPLAYSURF
-        self.SCREEN_WIDTH, self.SCREEN_HEIGHT = self.DISPLAYSURF.get_size()
-        self.speed = speed
 
-        self.surf = pygame.Surface((width, height))
+        # takes in the width and height of the game screen / scene
+        self.SCREEN_WIDTH, self.SCREEN_HEIGHT = self.DISPLAYSURF.get_size()
+        self.speed = speed  # determines the speed according to the parameter entered
+        self.passed = False
+        self.surf = pygame.Surface((width, height))  # determines the size of the surfable surface
+
         self.rect = self.surf.get_rect(center=center)
 
     # updates obstacle for exactly one tick
     def update(self):
+        # determines the speed of vertical movement only, to "accompany" the background
         self.rect.move_ip(0, self.speed)
-        if (self.rect.top > self.SCREEN_HEIGHT):
-            self.rect.top = 0
+        if (self.rect.top > self.SCREEN_HEIGHT):  # Condition to determine the placement of the obstacle after every tick
+            self.rect.top = 0  # determines the position of the top of the rectangle on the scene
+            # determines the position of the center of the rectangle on the scene
             self.rect.center = (random.randint(40, self.SCREEN_WIDTH - 40), 0)
+            self.passed = False
+
+    # return virtualcopy of self for gamestate
+    def get_virtual_copy(self):
+        virtual_obstacle = VirtualObstacle(self.DISPLAYSURF, self.speed,
+                                           self.surf.get_width(), self.surf.get_height(), self.rect.center)
+        return virtual_obstacle
 
 
 class Obstacle(VirtualObstacle):
