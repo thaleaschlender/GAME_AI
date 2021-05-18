f500992b7ea1c58b7abc4b81c3ba51d434f66a10
emiliosao <emilio_sao@icloud.com>
2021-05-18 22:46:06 +0200

    fixed merge? (STILL NOT WORKING)

--- Obstacle.py
+++ Obstacle.py
@@ -2,60 +2,28 @@ import random
 
 import pygame
 
-<<<<<<< HEAD
-#VIRTUAL OBSTACLE CLASS
-=======
 
->>>>>>> background
 class VirtualObstacle(pygame.sprite.Sprite):
     """Virtual parent class of obstacle that has all the same variables and functions
     except anything required with drawing to screen."""
 
-<<<<<<< HEAD
-    #constructor
-=======
->>>>>>> background
     def __init__(self, DISPLAYSURF, speed, width, height, center):
-        super().__init__() #initialise from the parent class
+        super().__init__()
         self.DISPLAYSURF = DISPLAYSURF
-<<<<<<< HEAD
-
-        self.SCREEN_WIDTH, self.SCREEN_HEIGHT = self.DISPLAYSURF.get_size() #takes in the width and height of the game screen / scene
-        self.speed = speed #determines the speed according to the parameter entered
-        self.passed = False
-        self.surf = pygame.Surface((width, height))  # determines the size of the surfable surface
-
-=======
         self.SCREEN_WIDTH, self.SCREEN_HEIGHT = self.DISPLAYSURF.get_size()
         self.speed = speed
 
         self.surf = pygame.Surface((width, height))
->>>>>>> background
         self.rect = self.surf.get_rect(center=center)
 
     # updates obstacle for exactly one tick
     def update(self):
-<<<<<<< HEAD
-        self.rect.move_ip(0, self.speed) #determines the speed of vertical movement only, to "accompany" the background
-        if (self.rect.top > self.SCREEN_HEIGHT): #Condition to determine the placement of the obstacle after every tick
-            self.rect.top = 0 #determines the position of the top of the rectangle on the scene
-            self.rect.center = (random.randint(40, self.SCREEN_WIDTH - 40), 0) #determines the position of the center of the rectangle on the scene
-            self.passed = False
-
-    #return virtualcopy of self for gamestate
-    def get_virtual_copy(self):
-        virtual_obstacle = VirtualObstacle(self.DISPLAYSURF, self.speed, 
-                            self.surf.get_width(), self.surf.get_height(), self.rect.center)
-        return virtual_obstacle
-=======
         self.rect.move_ip(0, self.speed)
         if (self.rect.top > self.SCREEN_HEIGHT):
             self.rect.top = 0
             self.rect.center = (random.randint(40, self.SCREEN_WIDTH - 40), 0)
 
->>>>>>> background
 
-#OBSTACLE CLASS taking a VIRTUAL OBSTACLE parameter
 class Obstacle(VirtualObstacle):
     # initalisitation of an obstacle object with its width, height and height_position as parameters
     def __init__(self, DISPLAYSURF, speed, width, height, height_position):
@@ -73,10 +41,6 @@ class Obstacle(VirtualObstacle):
         super().update()
 
     def draw(self):
-<<<<<<< HEAD
-        self.DISPLAYSURF.blit(self.image, self.rect)  #draws the surfable item of the obstacle as a rectangle
-
-=======
         self.DISPLAYSURF.blit(self.image, self.rect)
 
     # return virtualcopy of self for gamestate
@@ -84,4 +48,3 @@ class Obstacle(VirtualObstacle):
         virtual_obstacle = VirtualObstacle(self.DISPLAYSURF, self.speed,
                                            self.surf.get_width(), self.surf.get_height(), self.rect.center)
         return virtual_obstacle
->>>>>>> background
---
+++ main_copy.py
@@ -0,0 +1,170 @@
+# Imports
+import pygame
+import sys
+from pygame.locals import *
+import time
+import random
+
+# Import other classes
+from Background import Background
+from Clouds import Clouds
+from Obstacle import Obstacle
+from Player import Player
+from Coin import Coin
+
+pygame.init()
+
+# Setting up FPS
+FPS = 60
+FramePerSec = pygame.time.Clock()
+
+
+# Other Variables for use in the program
+BLACK = (0, 0, 0)  # defines the colour black in a variable
+WHITE = (255, 255, 255)  # defines the colour white in a variable
+SCREEN_WIDTH = 512  # defines the width of the game screen
+SCREEN_HEIGHT = 512  # defines the height of the game screen
+
+speed = 5  # defines the general speed of movement
+score = 0  # Variable representing the score (initialised at 0 for the beginning of the game)
+
+# Setting up Fonts
+FONT = pygame.font.SysFont("Verdana", 60)
+FONT_SMALL = pygame.font.SysFont("Verdana", 20)
+GAME_OVER = FONT.render("Game Over", True, WHITE)
+
+# Create a white screen
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+DISPLAYSURF.fill(WHITE)
+pygame.display.set_caption("Group 15")
+
+# Display the current game score
+
+
+def display_score(score):
+    # defines the font properties of the score
+    scoreText = FONT_SMALL.render(str(score), True, BLACK)
+    # displays the score in the top left corner of the screen
+    DISPLAYSURF.blit(scoreText, (10, 10))
+
+# End game
+# Delete all existing sprites and close window.
+
+
+def end_game(sprites, score):
+    # a sound can be played
+    # pygame.mixer.Sound('crash.wav').play()
+
+    finalScore = FONT.render(str(score), True, WHITE)
+
+    time.sleep(0.8)
+    # Fills screen with black, shows a black screen
+    DISPLAYSURF.fill(BLACK)
+    # Writes game over on the screen on top of the black screen
+    DISPLAYSURF.blit(GAME_OVER, (SCREEN_WIDTH/10, SCREEN_HEIGHT/4))
+
+    # Show the final score
+    DISPLAYSURF.blit(finalScore, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+
+    pygame.display.update()
+    for entity in sprites:
+        entity.kill()
+
+    # wait one and a half second
+    time.sleep(1.5)
+    pygame.quit()  # quit pygame
+    sys.exit()  # close the demo window
+
+# Check if the player has collided with a coin
+# Returns the score of the game
+
+
+def check_coin_collision(player, coins, score):
+    # Find the collided coin
+    collidedCoin = pygame.sprite.spritecollideany(P1, coins)
+    # Check if collided with any coins
+    if collidedCoin != None and collidedCoin.visible:
+        # Increase the score and reset coin to the top
+        score += 1
+        collidedCoin.setVisible(False)
+
+    return score
+
+
+# Setting up Sprites
+P1 = Player(DISPLAYSURF)  # defines the player object
+
+# defines the original obstacles object spawing when we enter a new "screen scene" in the game
+E1 = Obstacle(DISPLAYSURF, speed, 40, 70, 0)
+
+# E2 = Obstacle(SCREEN_WIDTH,SCREEN_HEIGHT/2,SPEED) #obstacle that could be used for spawning random obstacles (e.g. vortex)
+
+E3 = Obstacle(DISPLAYSURF, speed, 405, 50, -350)
+
+# Create coins
+C1 = Coin(DISPLAYSURF, speed, 20, 20, 700)
+#C2 = Coin(DISPLAYSURF, speed, 20, 20, -200)
+#C3 = Coin(DISPLAYSURF, speed, 20, 20, -400)
+#C4 = Coin(DISPLAYSURF, speed, 20, 20, -600)
+C1.setObstacles(E3, E1)
+#C2.setObstacles(E3, E1)
+#C3.setObstacles(E3, E1)
+#C4.setObstacles(E3, E1)
+
+back_ground = Background(DISPLAYSURF)  # defines the background object
+clouds = Clouds(DISPLAYSURF)
+
+# Creating Sprites Groups
+obstacles = pygame.sprite.Group()
+obstacles.add(E1)
+# obstacles.add(E2)
+obstacles.add(E3)
+
+coins = pygame.sprite.Group()
+coins.add(C1)
+# coins.add(C2)
+# coins.add(C3)
+# coins.add(C4)
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(E1)
+# all_sprites.add(E2)
+all_sprites.add(E3)
+all_sprites.add(C1)
+# all_sprites.add(C2)
+# all_sprites.add(C3)
+# all_sprites.add(C4)
+
+# Adding a new User event: how often to look for user event?
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+
+# Game Loop
+while True:
+
+    # Cycles through all occurring events
+    for event in pygame.event.get():
+        if event.type == INC_SPEED:
+            speed += 0.5
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    back_ground.update()
+    clouds.update()
+
+    # Display the score
+    display_score(score)
+
+    # To be run if collision occurs between Player and Obstacle
+    if pygame.sprite.spritecollideany(P1, obstacles):
+        end_game(all_sprites, score)
+
+    score = check_coin_collision(P1, coins, score)
+
+    # Moves and Re-draws all Sprites
+    all_sprites.update()
+
+    pygame.display.update()
+    FramePerSec.tick(FPS)
